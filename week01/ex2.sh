86897f83f6cddf2c1cd1b7840a46e69c14f47059
JustSomeDude2001 <kim.yaroslavik@gmail.com>
2021-08-21 21:51:00 +0500

    Adding exercise 2

---
+++ week01/ex2.sh
@@ -0,0 +1,10 @@
+# Note that the output file was moved to
+# the .sh file's directory for ease of use
+
+# mkdir ~/week01
+# cd ~/week01
+
+history > ex2.txt
+
+# NOTE: history seems to be callable only from terminal manually.
+# Hence, I added history manually by calling "history > ex2.txt" from terminal
